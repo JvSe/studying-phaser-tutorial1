af61122e0761893e9626bfa7ee8587501d61c0d6
JvSe <joaovitosoares12@gmail.com>
2020-04-22 21:32:20 -0300

    Finalizando configurações para rodar no heroku

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express()
 
 app.use(express.static('src'));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/src/index.html"))
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000 )
